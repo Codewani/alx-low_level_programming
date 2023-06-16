556207ee9396ca325258cc57b4366efc4060faeb
Codewani <kondwanimwape013@gmail.com>
2023-06-16 06:51:53 +0000

    Hello World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main - demonstrates the usage of the puts() function.
+ * @void: no parameters accepted
+ * It prints a string to the console.
+ * Return: 0  Program executed successfully.
+ */
 int main(void)
-/* print a statement using puts */
 {
 	char Mystr[] = "\"Programming is like building a multilingual puzzle";
 
