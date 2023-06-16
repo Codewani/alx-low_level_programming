b4f1ebbff532ebf6f2e4b42dcda0a2cdc362947a
Codewani <kondwanimwape013@gmail.com>
2023-06-16 07:10:11 +0000

    sizeofdatatypes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,10 +15,10 @@ int main(void)
 
 	long long int d;
 
-	printf("Size of a char: %zu byte(s)", sizeof(b));
-	printf("Size of an int: %zu byte(s)", sizeof(a));
-	printf("Size of a long: %zu byte(s)", sizeof(c));
-	printf("Size of a long long int: %zu byte(s)", sizeof(d));
+	printf("Size of a char: %zu byte(s)\n", sizeof(b));
+	printf("Size of an int: %zu byte(s)\n", sizeof(a));
+	printf("Size of a long: %zu byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
 
 	return (0);
 }
