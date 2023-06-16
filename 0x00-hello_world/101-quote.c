58abf3677952106aedb870a295ba47ac28da6097
Codewani <kondwanimwape013@gmail.com>
2023-06-16 07:45:31 +0000

    writeFunction

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+ *main - demonstrates the usage of the write() function.
+ *@void: no parameters accepted
+ *It prints a string to the console.
+ *Return: 0  Program executed successfully.
+ */
+int main(void)
+{
+	char Mystr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	write(STDOUT_FILENO, Mystr, sizeof(Mystr) - 1);
+
+	return (0);
+}
