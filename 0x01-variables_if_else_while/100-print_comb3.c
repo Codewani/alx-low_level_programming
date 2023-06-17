e43fe2979b25492d76c0c6cfed78ffd005e93fac
Codewani <kondwanimwape013@gmail.com>
2023-06-17 17:57:30 +0000

    commaAfterNumber

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *main - point of execution.
+ *@void: no parameters accepted
+ *Prints the alphabet.
+ *Return: 0  Program executed successfully.
+ */
+int main(void)
+{
+	int i;
+
+	int j;
+
+	int k;
+
+
+	for (i = 0; a <= 9; i++)
+		for (j = i + 1; j < 9; j++)
+		{
+			putchar(48 + i);
+			putchar(48 + j);
+		}
+	putchar('\n');
+
+	return (0);
+}
