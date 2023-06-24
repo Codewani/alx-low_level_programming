aa71f5f465d43618c3538a2c1c93e76059ac50e8
Codewani <kondwanimwape013@gmail.com>
2023-06-24 19:21:44 +0000

    main.h

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+nclude <stdio.h>
+/**
+ *  *main - demonstrates the usage of the puts() function.
+ *   *@void: no parameters accepted
+ *    *It prints a string to the console.
+ *     *Return: 0  Program executed successfully.
+ *      */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+void print_alphabet(void){
+	for (a = 0; a <= 25; a++)
+		putchar(97 + a);
+	putchar('\n');
+}
