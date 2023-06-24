1f84366b989b0279b9eb0f464478c826f6b5bb09
Codewani <kondwanimwape013@gmail.com>
2023-06-24 20:40:06 +0000

    alphabetfunc

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,14 +9,13 @@
 void print_alphabet_x10(void)
 {
 	int a;
-	
+
 	int b;
 
-	for(b = 0; b <=9; b++)
+	for(b = 0; b <= 9; b++)
 	{
 		for (a = 0; a <= 25; a++)
 			putchar(97 + a);
 		putchar('\n');
 	}
-	putchar('\n');
 }
