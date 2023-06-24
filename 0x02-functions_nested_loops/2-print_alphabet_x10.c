13477ed7a2b47b1c0f6994acd2af9891fcccb36b
Codewani <kondwanimwape013@gmail.com>
2023-06-24 20:38:05 +0000

    alphabetfunc

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_alphabet_x10 - prints the alphabet 10 times
+ *@void: no parameters accepted
+ *It prints a string to the console.
+ *Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int a;
+	
+	int b;
+
+	for(b = 0; b <=9; b++)
+	{
+		for (a = 0; a <= 25; a++)
+			putchar(97 + a);
+		putchar('\n');
+	}
+	putchar('\n');
+}
