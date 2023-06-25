cd9b04683cc3c2903709bd56e03e659f6cf82e6d
Codewani <kondwanimwape013@gmail.com>
2023-06-25 21:42:11 +0000

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *times_table - Tells you whether the parameter is lower case or not
+ *Return: Noting.
+ */
+void times_table(void)
+{
+	int i;
+
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			printf("%d\n", i * j);
+			if (j != 9)
+				putchar(44);
+		}
+	}
+}
