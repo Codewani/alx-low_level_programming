e4584460b3b4166e64573e75f16905f06443afbe
Codewani <kondwanimwape013@gmail.com>
2023-06-25 22:13:12 +0000

    9-times_table.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,7 +2,8 @@
 #include "main.h"
 /**
  *add - Tells you whether the parameter is lower case or not
- *@c, d: accepts integers c and d
+ *@c: accepts integers c and d
+ *@d: accepts integers c and d
  *Return: the sum of and c and d
  */
 int add(int c, int d)
