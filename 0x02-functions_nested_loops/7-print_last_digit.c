004fb57c2d12caf8b1ee1962eb26ab236cea1f59
Codewani <kondwanimwape013@gmail.com>
2023-06-25 09:25:52 +0000

    6-abs.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_last_digit - Tells you whether the parameter is lower case or not
+ *@c: accepts an integer c
+ *It prints a string to the console.
+ *Return: The last digit of c..
+ */
+int print_last_digit(int c)
+{
+	return (c % 10);
+}
