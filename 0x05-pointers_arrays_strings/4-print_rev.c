5f604f8ac2b3deadadb98817f010266d672b5f25
Codewani <kondwanimwape013@gmail.com>
2023-06-26 12:50:09 +0000

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_rev - check the code
+ *
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+	int len;
+
+	int i;
+
+	len = strlen(s);
+	for (i = len - 1; i >= 0; i--)
+		printf("%c", s[i]);
+	putchar('\n');
+}
