2d07f08791e69210d9af3ef10af5c861968d3ce4
Codewani <kondwanimwape013@gmail.com>
2023-06-29 14:53:25 +0000

    concat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_strcat - check the code
+ *@s: pointer to a str
+ *Return: None.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+        return (strcar(dest, src));
+}
