7a6981553c0f267e163013fb49dfca1ff694fe13
Codewani <kondwanimwape013@gmail.com>
2023-07-03 11:46:44 +0000

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,7 +5,7 @@
  *@src: memory where is copied
  *@n: number of bytes
  *
- *Return: copied memory with n bytes changed
+ *Return: copied memory
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
