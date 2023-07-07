9d6d2b2e32e4fdf177fbfd7d0dae073bfd264b95
Codewani <kondwanimwape013@gmail.com>
2023-07-07 07:08:15 +0000

    1-print_rev_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+//int actual_sqrt_recursion(int n, int i);
+/**
+ * _sqrt_recursion - computes the natural square root of a number
+ * @n: number
+ *
+ * Return: square root of n
+ */
+int _sqrt_recursion(int n)
+{
+	int origin = n;
+	if (n == 1)
+		return (1);
+	else if (n == 0)
+		return (0);
+	else
+	if (n * n != origin)
+		
