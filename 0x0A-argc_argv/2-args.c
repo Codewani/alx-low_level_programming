27307915d2dfc1fa40a2739d356e700c3c72ca82
Codewani <kondwanimwape013@gmail.com>
2023-07-08 19:01:01 +0000

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints all arguments it receives
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int p;
+
+	for (p = 0; i < argc; p++)
+	{
+		printf("%s\n", argv[p]);
+	}
+
+	return (0);
+}
